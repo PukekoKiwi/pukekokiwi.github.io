19ffe17448cbbbe93f4f2759c2ae537a708055a3
PukekoKiwi <liamryana@gmail.com>
2018-01-09 01:24:53 +0000

    Added more content to tab section

--- script.js
+++ script.js
@@ -15,5 +15,3 @@ function openTab(evt, tabName) {
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
-
-window.onload = openTab(event, 'About');
